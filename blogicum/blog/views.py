71158966abe2ecb4832d1522a0291bc6ede9a31d
Гилязова Альбина <giliazova@mail.ru>
2024-03-09 01:14:55 +0300

    Fix on review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseNotFound
 
 # Временная заглушка для базы данных.
-posts: list = [
+posts: list[dict] = [
     {
         'id': 0,
         'location': 'Остров отчаянья',
